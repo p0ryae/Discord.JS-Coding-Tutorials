ab1308c5eefdcbde0a958764a3300d0029c49eb1
Poryafm12 <59381835+Poryafm12@users.noreply.github.com>
2020-04-09 14:04:18 -0700

    1.0

--- index.js
+++ index.js
@@ -3,4 +3,27 @@ const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online`)
+    bot.user.setActivity("DashCraft", {type: ""});
+})
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ")
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hi`){
+        return message.channel.send("Hello")
+    }
+
+    if(cmd === `${prefix}dashcraft`){
+        return message.reply("Is good!")
+    }
+
+})
+
 bot.login(botsettings.token);
