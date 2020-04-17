45b59d44705271a36c9db5756ff9a37553585855
Poryafm12 <59381835+Poryafm12@users.noreply.github.com>
2020-04-17 14:18:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
 
     let prefix = botsettings.prefix;
-    let messageArray = message.content.split(" ")
+    let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
@@ -20,8 +20,29 @@ bot.on("message", async message => {
         return message.channel.send("Hello")
     }
 
-    if(cmd === `${prefix}dashcraft`){
-        return message.reply("Is good!")
+    if(cmd === `${prefix}clear`){
+        if (message.deletable) {
+            message.delete();
+        }
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Missing Permissions!").then(m => m.delete(5000));
+        }
+
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("This is not a number").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+        .catch(err => message.reply(`Something went wrong... ${err}`));
+    
     }
 
 })
