ca9c10135cade13c07a347d3f27970cb0b156492
Poryafm12 <59381835+Poryafm12@users.noreply.github.com>
2020-05-19 16:00:26 -0700

    Update botsettings.json

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("guildMemberAdd", member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
-    welcomeChannel.send (`Welcome! ${member}`)
-})
 
-bot.on("guildMemberRemove", member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'goodbye')
-    welcomeChannel.send (`Goodbye! ${member}`)
-})
 
 require("./util/eventHandler")(bot)
 
