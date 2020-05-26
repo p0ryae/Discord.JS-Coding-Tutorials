e11e39d5bb34f8d17b37275c598eb3a5cbe47773
Poryafm12 <59381835+Poryafm12@users.noreply.github.com>
2020-05-25 17:32:00 -0700

    Updated #7

--- commands/mute.js
+++ commands/mute.js
@@ -6,8 +6,8 @@ module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
 
-        let mutedRole = message.guild.roles.cache.get('714603154192203827');
-        let verifiedRole = message.guild.roles.cache.get('714603048118255709');
+        let mutedRole = message.guild.roles.cache.get('MUTE_ROLE_ID');
+        let verifiedRole = message.guild.roles.cache.get('ROLE ID (IF YOU ALREADY HAVE A ROLE DEAFULT FOR PEOPLE WHO JOINS YOUR SERVER!)');
         if(mutedRole) {
             member.roles.add(mutedRole);
             member.roles.remove(verifiedRole);
