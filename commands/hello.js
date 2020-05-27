f2b22d4e692d53cf37f0562bf285f721703b42cb
Poryafm12 <59381835+Poryafm12@users.noreply.github.com>
2020-05-27 02:52:04 -0700

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -2,26 +2,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    if(args.length === 2) {
-        const member = message.mentions.members.size === 1 ? 
-          message.mentions.members.first() :
-          message.guild.members.cache.get(args[1]);
-        if(member) {
-          const embed = new MessageEmbed()
-            .setTitle('User Information')
-              .addField('Player Name', message.author.username, true)
-              .addField('Version ', version, true)
-              .addField('Current Server', message.guild.name, true)
-              .addField(`Owner of ${message.guild.name}`, message.guild.owner, true)
-              .addField('Latency', `\n**Ping:** \`${responseTime}ms\``, true)
-              .setColor(0xE1310C)
-              .setThumbnail(message.author.displayAvatarURL({dynamic: true, format: 'png'}))
-              .setDescription(`${member.roles.cache.map(role => role.toString()).join(' ')}`)
-              .setTimestamp()
-              .setFooter('Looking pretty slick')
-          message.channel.send(embed);
-    }
-}
+      message.reply('Sup')
 }
 
 module.exports.config = {
